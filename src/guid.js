530eb2b6cb80ec0e2253370f207ca1cb318e680c
Yoonji Jang <yjang@letsee.io>
2017-07-18 20:57:32 +0900

    utils

---
+++ src/guid.js
@@ -0,0 +1,5 @@
+// @flow
+const s4 = (): string => ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+const guid = (): string => `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+
+export default guid;
