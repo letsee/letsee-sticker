f5ffbcd5da6a0285988ec7aed4b2bd18d2e1d998
Yu Myungjae <u4bi@letsee.io>
2017-07-19 12:29:33 +0900

    emojibox.js 함수 추가
    - createEmojiBox()
    - scrollClick()
    - closeAddEmoji()

--- src/js/app.js
+++ src/js/app.js
@@ -176,30 +176,6 @@
 
     }      
 
-    function scrollClick() {
-        var 
-            wrapper = document.querySelector('.emoji-box-wrapper');
-        
-        document.querySelector('.emoji-box-content').style.overflow = wrapper.classList.toString() === 'emoji-box-wrapper' ? 'scroll' : 'hidden';
-
-        var ctrl = wrapper.classList.toString() === 'emoji-box-wrapper' ? false : true;
-
-        manager.get('pan').set({ enable : ctrl });
-        manager.get('pinch').set({ enable : ctrl });
-        manager.get('rotate').set({ enable : ctrl });
-
-        document.querySelector('.emoji-box-content').scrollTop = 0;
-        wrapper.classList.toggle('transform-active');
-    }
-
-    function closeAddEmoji() {
-        if (document.querySelector('.emoji-box-wrapper').classList.toString() !== 'emoji-box-wrapper') scrollClick();
-        
-        document.querySelector('.ar-renderer').removeEventListener('touchend', closeAddEmoji);          
-        typeLoadControl(1);
-
-    }
-
     function createDOMRanderable(value, className, clickCallback) {
 
     var
--- src/js/emojibox.js
+++ src/js/emojibox.js
@@ -73,3 +73,37 @@
         document.querySelector('.emoji-box-item').appendChild(document.createElement('br'));
         document.querySelector('.emoji-box-item').appendChild(document.createElement('br'));
     }
+
+
+
+
+
+
+    function scrollClick() {
+        var 
+            wrapper = document.querySelector('.emoji-box-wrapper');
+        
+        document.querySelector('.emoji-box-content').style.overflow = wrapper.classList.toString() === 'emoji-box-wrapper' ? 'scroll' : 'hidden';
+
+        var ctrl = wrapper.classList.toString() === 'emoji-box-wrapper' ? false : true;
+
+        manager.get('pan').set({ enable : ctrl });
+        manager.get('pinch').set({ enable : ctrl });
+        manager.get('rotate').set({ enable : ctrl });
+
+        document.querySelector('.emoji-box-content').scrollTop = 0;
+        wrapper.classList.toggle('transform-active');
+    }
+
+
+
+
+
+
+    function closeAddEmoji() {
+        if (document.querySelector('.emoji-box-wrapper').classList.toString() !== 'emoji-box-wrapper') scrollClick();
+        
+        document.querySelector('.ar-renderer').removeEventListener('touchend', closeAddEmoji);          
+        typeLoadControl(1);
+
+    }    
