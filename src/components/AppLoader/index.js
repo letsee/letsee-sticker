4a885fdb63b8eee8e5e8877b8ba38a4da5e20475
Yoonji Jang <yjang@letsee.io>
2017-07-20 15:46:53 +0900

    app loader

--- src/components/AppLoader/index.js
+++ src/components/AppLoader/index.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
-import classNames from 'classnames';
-import styles from './styles.scss';
+import styled from 'styled-components';
+import Container from '../Container';
 
 import leftTop from './frame-b-lt.png';
 import leftTop2x from './frame-b-lt@2x.png';
@@ -19,48 +19,84 @@ import rightBottom from './frame-b-rb.png';
 import rightBottom2x from './frame-b-rb@2x.png';
 import rightBottom3x from './frame-b-rb@3x.png';
 
+const Frame = styled.img`
+  position: absolute;
+  user-select: none;
+`;
+
+const FrameLeftTop = Frame.extend`
+  left: 45px;
+  top: 78px;
+`;
+
+const FrameRightTop = Frame.extend`
+  right: 45px;
+  top: 78px;
+`;
+
+const FrameLeftBottom = Frame.extend`
+  left: 45px;
+  bottom: 78px;
+`;
+
+const FrameRightBottom = Frame.extend`
+  right: 45px;
+  bottom: 78px;
+`;
+
+const Text = styled.div`
+  user-select: none;
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  opacity: 0.8;
+  font-size: 20px;
+  font-weight: bold;
+  letter-spacing: -0.8px;
+  color: #fff;
+  text-shadow: 0 0 12px rgba(0, 0, 0, 0.5);
+`;
+
 type PropTypes = {
-  className?: string, // eslint-disable-line react/require-default-props
   children?: any, // eslint-disable-line react/require-default-props
 };
 
-const AppLoader = ({ children, className, ...other }: PropTypes) => (
-  <div className={classNames('container', styles.appLoader, className)} {...other}>
-    <div className={styles.wrapper}>
-      <img
-        className={classNames(styles.frame, styles.leftTop)}
+const AppLoader = ({ children, ...other }: PropTypes) => (
+  <Container {...other}>
+    <div>
+      <FrameLeftTop
         src={leftTop}
         srcSet={`${leftTop2x} 2x, ${leftTop3x} 3x`}
         alt="스티커 메세지를 남길 제품을 비춰주세요"
       />
 
-      <img
-        className={classNames(styles.frame, styles.rightTop)}
+      <FrameRightTop
         src={rightTop}
         srcSet={`${rightTop2x} 2x, ${rightTop3x} 3x`}
         alt="스티커 메세지를 남길 제품을 비춰주세요"
       />
 
-      <img
-        className={classNames(styles.frame, styles.leftBottom)}
+      <FrameLeftBottom
         src={leftBottom}
         srcSet={`${leftBottom2x} 2x, ${leftBottom3x} 3x`}
         alt="스티커 메세지를 남길 제품을 비춰주세요"
       />
 
-      <img
-        className={classNames(styles.frame, styles.rightBottom)}
+      <FrameRightBottom
         src={rightBottom}
         srcSet={`${rightBottom2x} 2x, ${rightBottom3x} 3x`}
         alt="스티커 메세지를 남길 제품을 비춰주세요"
       />
     </div>
 
-    <div className={styles.text}>
+    <Text>
       <div>스티커 메세지를 남길</div>
       <div>제품을 비춰주세요</div>
-    </div>
-  </div>
+    </Text>
+  </Container>
 );
 
 export default AppLoader;
