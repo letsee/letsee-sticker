1ff95199eb04df42163c1ec53bd68d4651e99152
Yoonji Jang <yjang@letsee.io>
2017-07-21 15:05:35 +0900

    app

---
+++ src/components/App.js
@@ -0,0 +1,120 @@
+// @flow
+import React from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import './App.scss';
+import AppLoader from './AppLoader';
+import Entity from './Entity';
+import MessageForm from './MessageForm';
+import {
+  initMessageForm,
+  clearMessageForm,
+  destroyMessageForm,
+  submitMessageForm,
+  selectSticker,
+  deselectSticker,
+  deleteSticker,
+  addSticker,
+} from '../actions';
+
+const openCapture = () => window._app && window._app.openCapture && window._app.openCapture();
+
+const Main = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+`;
+
+const App = ({
+  entities,
+  currentEntity,
+  selectedSticker,
+  stickers,
+  messageForm,
+  dispatch,
+}) => {
+  if (messageForm !== null) {
+    const entityTracked = currentEntity !== null && messageForm.uri === currentEntity;
+    const messageEntity = entities.byUri[messageForm.uri];
+    const selectedStickerData = stickers.byId[selectedSticker];
+    const stickersById = stickers.allIds
+      .map(id => stickers.byId[id])
+      .filter(sticker => sticker && sticker.entityUri === messageForm.uri);
+
+    return (
+      <Main>
+        <MessageForm
+          entity={messageEntity}
+          stickers={stickersById}
+          selectedSticker={selectedStickerData}
+          entityTracked={entityTracked}
+          submitting={messageForm.submitting}
+          onClose={() => {
+            dispatch(destroyMessageForm(messageForm.uri));
+            dispatch(clearMessageForm(messageForm.uri, stickersById.map(sticker => sticker.id)));
+          }}
+          nextDisabled={stickersById.length === 0 || messageForm.submitting}
+          onNext={() => dispatch(submitMessageForm(messageForm.uri))}
+          onCaptureClick={openCapture}
+          onStickerClick={id => dispatch(selectSticker(id))}
+          onTextInput={(value) => {
+            const action = dispatch(addSticker(messageForm.uri, value));
+
+            if (entityTracked) {
+              dispatch(selectSticker(action.payload.id));
+            }
+          }}
+          onEmojiInput={(value) => {
+            const action = dispatch(addSticker(messageForm.uri, value));
+
+            if (entityTracked) {
+              dispatch(selectSticker(action.payload.id));
+            }
+          }}
+          onTransformationComplete={() => dispatch(deselectSticker(selectedSticker))}
+          onDelete={() => dispatch(deleteSticker(selectedSticker))}
+        />
+      </Main>
+    );
+  }
+
+  if (currentEntity !== null) {
+    const currentEntityData = entities.byUri[currentEntity];
+
+    return (
+      <Main>
+        <Entity
+          data={currentEntityData}
+          onNewClick={() => dispatch(initMessageForm(currentEntity))}
+          onCaptureClick={openCapture}
+        />
+      </Main>
+    );
+  }
+
+  return (
+    <Main>
+      <AppLoader />
+    </Main>
+  );
+};
+
+export default connect(
+  ({
+    letseeLoaded,
+    currentEntity,
+    currentUser,
+    stickers,
+    entities,
+    selectedSticker,
+    messageForm,
+  }) => ({
+    letseeLoaded,
+    currentEntity,
+    currentUser,
+    stickers,
+    entities,
+    selectedSticker,
+    messageForm,
+  }),
+)(App);
