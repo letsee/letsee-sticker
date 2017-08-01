53edc0c710d705d3fb83463d5fb8cb616b75f5b3
Yoonji Jang <yjang@letsee.io>
2017-08-01 23:37:17 +0900

    gray close button

--- src/components/CloseButton/index.js
+++ src/components/CloseButton/index.js
@@ -6,7 +6,12 @@ import closeIcon from './btn-close.png';
 import closeIcon2x from './btn-close@2x.png';
 import closeIcon3x from './btn-close@3x.png';
 
+import grayCloseIcon from './btn-close-g.png';
+import grayCloseIcon2x from './btn-close-g@2x.png';
+import grayCloseIcon3x from './btn-close-g@3x.png';
+
 const CloseButton = ({
+  gray,
   children,
   ...other
 }) => (
@@ -14,12 +19,24 @@ const CloseButton = ({
     type="button"
     {...other}
   >
-    <img
-      alt="Close"
-      src={closeIcon}
-      srcSet={`${closeIcon2x} 2x, ${closeIcon3x} 3x`}
-    />
+    {gray ? (
+      <img
+        alt="Close"
+        src={grayCloseIcon}
+        srcSet={`${grayCloseIcon2x} 2x, ${grayCloseIcon3x} 3x`}
+      />
+    ) : (
+      <img
+        alt="Close"
+        src={closeIcon}
+        srcSet={`${closeIcon2x} 2x, ${closeIcon3x} 3x`}
+      />
+    )}
   </ImageButton>
 );
+
+CloseButton.defaultProps = {
+  gray: false,
+};
 
 export default CloseButton;
