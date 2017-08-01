1bc72e74bae35d527f7eedc38c2e57558549becd
Yoonji Jang <yjang@letsee.io>
2017-08-01 23:37:50 +0900

    news item

---
+++ src/components/NewsList/NewsItem.js
@@ -0,0 +1,56 @@
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+import moment from 'moment';
+
+const Container = styled.div`
+  zoom: 1;
+  overflow: hidden;
+  padding: 12px;
+  box-shadow: inset 0 -0.5px 0 0 #e9e9e9;
+`;
+
+const ImageContainer = styled.div`
+  display: table-cell;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding-right: 14px;
+`;
+
+const Image = styled.img`
+  width: 54px;
+  height: 54px;
+  object-fit: contain;
+  display: block;
+`;
+
+const Message = styled.div`
+  zoom: 1;
+  overflow: hidden;
+  display: table-cell;
+  vertical-align: middle;
+  font-family: AppleSDGothicNeo, sans-serif;
+  font-size: 15px;
+  letter-spacing: -0.2px;
+  color: #000;
+  word-wrap: break-word;
+`;
+
+const NewsItem = ({
+  data: { image, description, timestamp },
+  children,
+  ...other
+}) => (
+  <Container {...other}>
+    <ImageContainer>
+      <Image src={image} />
+    </ImageContainer>
+
+    <Message>
+      <div>{moment(timestamp).format('YYYY. M. D.')}</div>
+      <div>{description}</div>
+    </Message>
+  </Container>
+);
+
+export default NewsItem;
