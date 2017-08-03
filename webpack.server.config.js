de8de411cfa9d46e9ec4ac8bd827d57a1b62ab77
Yoonji Jang <yjang@letsee.io>
2017-08-03 14:52:14 +0900

    new build config

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const dotenv = require('dotenv');
+const DotenvWebpack = require('dotenv-webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const env = process.env.NODE_ENV || 'production';
-const isDev = env !== 'production' && env !== 'staging';
+const envFile = './.env';
+dotenv.config({ path: envFile });
+const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: {
@@ -17,7 +21,7 @@ module.exports = {
   externals: fs.readdirSync(path.resolve(__dirname, 'node_modules')).concat([
     'react-dom/server',
   ]).reduce(function (ext, mod) {
-    ext[mod] = 'commonjs ' + mod;
+    ext[mod] = `commonjs ${mod}`;
     return ext;
   }, {}),
   node: {
@@ -33,7 +37,16 @@ module.exports = {
       },
     ],
   },
-  plugins: isDev ? [] : [
+  plugins: isDev ? [
+    new CleanWebpackPlugin(['server.bundle.js']),
+    new DotenvWebpack({
+      path: envFile,
+    }),
+  ] : [
+    new CleanWebpackPlugin(['server.bundle.js']),
+    new DotenvWebpack({
+      path: envFile,
+    }),
     new UglifyJSPlugin({
       comments: false,
     }),
