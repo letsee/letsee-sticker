6c39be932fdb4e8cdce8f6247f7e3e474904258a
Yoonji Jang <yjang@letsee.io>
2017-08-03 19:30:32 +0900

    gray spinner

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,9 +1,15 @@
+// @flow
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled, { keyframes } from 'styled-components';
 
 import spin from './idc-loading-t.png';
 import spin2x from './idc-loading-t@2x.png';
 import spin3x from './idc-loading-t@3x.png';
+
+import graySpin from './idc-loading-t-g.png';
+import graySpin2x from './idc-loading-t-g@2x.png';
+import graySpin3x from './idc-loading-t-g@3x.png';
 
 const spinAnimation = keyframes`
   to {
@@ -18,12 +24,25 @@ const SpinnerImage = styled.img`
   animation: 0.8s ${spinAnimation} infinite linear;
 `;
 
-const Spinner = () => (
+type SpinnerPropTypes = {
+  gray?: boolean,
+};
+
+const Spinner = ({ gray, ...other }: SpinnerPropTypes) => (
   <SpinnerImage
     alt="Loading.."
-    src={spin}
-    srcSet={`${spin2x} 2x, ${spin3x} 3x`}
+    src={gray ? graySpin : spin}
+    srcSet={`${gray ? graySpin2x : spin2x} 2x, ${gray ? graySpin3x : spin3x} 3x`}
+    {...other}
   />
 );
+
+Spinner.propTypes = {
+  gray: PropTypes.bool,
+};
+
+Spinner.defaultProps = {
+  gray: false,
+};
 
 export default Spinner;
