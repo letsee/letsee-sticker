34aa6ad3adb2b0953ca6abfd8d62f7acaf419cd7
Yoonji Jang <yjang@letsee.io>
2017-08-09 12:58:47 +0900

    emoji, text large button images

--- src/components/AddEmojiButton/index.js
+++ src/components/AddEmojiButton/index.js
@@ -2,10 +2,6 @@
 import React from 'react';
 import { ImageButton } from '../Button';
 
-import emojiIcon from './group-2-copy-3.png';
-import emojiIcon2x from './group-2-copy-3@2x.png';
-import emojiIcon3x from './group-2-copy-3@3x.png';
-
 type AddEmojiButtonPropTypes = {
   small: boolean,
 };
@@ -22,17 +18,20 @@ const AddEmojiButton = ({
     {small ? (
       <img
         alt="Add Emoji"
-        src="https://res.cloudinary.com/df9jsefb9/image/upload/s--Nic5mPel--/c_scale,h_54,q_auto/v1502250483/assets/btn-add-emoji_3x.png"
+        src="https://res.cloudinary.com/df9jsefb9/image/upload/s--Nic5mPel--/c_scale,w_54,q_auto/v1502250483/assets/btn-add-emoji_3x.png"
         srcSet="
-          https://res.cloudinary.com/df9jsefb9/image/upload/s--Nic5mPel--/c_scale,h_108,q_auto/v1502250483/assets/btn-add-emoji_3x.png 2x,
-          https://res.cloudinary.com/df9jsefb9/image/upload/s--Nic5mPel--/c_scale,h_162,q_auto/v1502250483/assets/btn-add-emoji_3x.png 3x
+          https://res.cloudinary.com/df9jsefb9/image/upload/s--Nic5mPel--/c_scale,w_108,q_auto/v1502250483/assets/btn-add-emoji_3x.png 2x,
+          https://res.cloudinary.com/df9jsefb9/image/upload/s--Nic5mPel--/c_scale,w_162,q_auto/v1502250483/assets/btn-add-emoji_3x.png 3x
         "
       />
     ) : (
       <img
         alt="Add Emoji"
-        src={emojiIcon}
-        srcSet={`${emojiIcon2x} 2x, ${emojiIcon3x} 3x`}
+        src="https://res.cloudinary.com/df9jsefb9/image/upload/s--nnCHGEWM--/c_scale,h_110,q_auto/v1502250483/assets/group-2-copy-3_3x.png"
+        srcSet="
+          https://res.cloudinary.com/df9jsefb9/image/upload/s--nnCHGEWM--/c_scale,h_220,q_auto/v1502250483/assets/group-2-copy-3_3x.png 2x,
+          https://res.cloudinary.com/df9jsefb9/image/upload/s--nnCHGEWM--/c_scale,h_330,q_auto/v1502250483/assets/group-2-copy-3_3x.png 3x,
+        "
       />
     )}
   </ImageButton>
