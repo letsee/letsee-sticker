f3ffa08e98f71a167e1dd0388c3b5f13e4de411c
Yoonji Jang <yjang@letsee.io>
2017-08-09 13:04:26 +0900

    entity images

--- src/components/Entity/index.js
+++ src/components/Entity/index.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { renderToString } from 'react-dom/server';
+import { render } from 'react-dom';
 import styled from 'styled-components';
 import clamp from 'lodash/clamp';
 import { ImageButton } from '../Button';
@@ -9,22 +9,26 @@ import {
   MIN_DIAGONAL,
 } from '../../constants';
 
-import createIcon from './btn-create.png';
-import createIcon2x from './btn-create@2x.png';
-import createIcon3x from './btn-create@3x.png';
-
-import addIcon from './btn-add-content.png';
-import addIcon2x from './btn-add-content@2x.png';
-import addIcon3x from './btn-add-content@3x.png';
+const ARContainer = styled.div`
+  position: relative;
+`;
 
 const StyledImageButton = ImageButton.extend`
-  position: absolute;
-  right: 2px;
-  bottom: 79px;
   margin: 0 auto;
 `;
 
+const StyledImageButtonRight = StyledImageButton.extend`
+  position: absolute;
+  right: 2px;
+  bottom: 79px;
+`;
+
 const MessageText = styled.div`
+  position: absolute;
+  white-space: nowrap;
+  left: 50%;
+  bottom: ${props => props.height}px;
+  transform: translateX(-50%);
   opacity: 0.8;
   font-family: 'Noto Sans KR Black', AppleSDGothicNeo, sans-serif;
   font-size: ${props => props.size * 0.06}px;
@@ -48,7 +52,16 @@ type EntityPropTypes = {
 };
 
 class Entity extends Component {
-  componentWillMount() {
+  constructor(props: EntityPropTypes) {
+    super(props);
+
+    if (typeof letsee !== 'undefined' && letsee !== null) {
+      const container = document.createElement('div');
+      this.object = new DOMRenderable(container);
+    }
+  }
+
+  componentDidMount() {
     this.renderAR(this.props);
   }
 
@@ -59,91 +72,75 @@ class Entity extends Component {
   componentWillUnmount() {
     if (typeof letsee !== 'undefined' && letsee !== null) {
       const entity = letsee.getEntity(this.props.data.uri);
-
-      entity.object.children.forEach((child) => {
-        if (child instanceof DOMRenderable) {
-          child.element.removeEventListener('click', this.props.onNewClick);
-        }
-
-        entity.object.remove(child);
-      });
-
-      entity.removeRenderables();
+      entity.removeRenderable(this.object);
     }
   }
 
   props: EntityPropTypes;
 
   renderAR({ data, onNewClick }: EntityPropTypes) {
-    const { name, uri, size: { width, height, depth } } = data;
-    const entity = letsee.getEntity(uri);
+    if (typeof letsee !== 'undefined' && letsee !== null) {
+      const { name, uri, size: { width, height, depth } } = data;
+      const entity = letsee.getEntity(uri);
 
-    entity.object.children.forEach((child) => {
-      if (child instanceof DOMRenderable) {
-        child.element.removeEventListener('click', onNewClick);
+      if (this.object.parent !== entity.object) {
+        entity.addRenderable(this.object);
       }
 
-      entity.object.remove(child);
-    });
+      let realDiagonal = MAX_DIAGONAL;
 
-    entity.removeRenderables();
+      if (typeof width !== 'undefined' && width !== null && typeof height !== 'undefined' && height !== null) {
+        realDiagonal = Math.sqrt((width * width) + (height * height));
+      }
 
-    let realDiagonal = MAX_DIAGONAL;
+      const diagonal = clamp(realDiagonal, MIN_DIAGONAL, MAX_DIAGONAL);
+      const realToClamped = realDiagonal / diagonal;
 
-    if (typeof width !== 'undefined' && width !== null && typeof height !== 'undefined' && height !== null) {
-      realDiagonal = Math.sqrt((width * width) + (height * height));
+      if (realDiagonal !== diagonal) {
+        this.object.scale.setScalar(realToClamped);
+      }
+
+      if (depth !== null && typeof depth !== 'undefined') {
+        this.object.position.setZ(depth / 2);
+      }
+
+      const buttonSize = diagonal * 0.33;
+      const nearest = Math.ceil(buttonSize / 100) * 100;
+      const y = (buttonSize + height / realToClamped) / 2 + diagonal * 0.04;
+
+      render(
+        <ARContainer>
+          <MessageText
+            size={diagonal}
+            height={y}
+          >
+            <div>
+              {name}에
+            </div>
+
+            <div>
+              스티커 메세지를 남겨보세요
+            </div>
+          </MessageText>
+
+          <StyledImageButton
+            type="button"
+            onClick={onNewClick}
+          >
+            <img
+              src={`https://res.cloudinary.com/df9jsefb9/image/upload/c_scale,h_${nearest},q_auto/v1501870222/assets/btn-add-content_3x.png`}
+              srcSet={`
+                https://res.cloudinary.com/df9jsefb9/image/upload/c_scale,h_${nearest * 2},q_auto/v1501870222/assets/btn-add-content_3x.png 2x,
+                https://res.cloudinary.com/df9jsefb9/image/upload/c_scale,h_${nearest * 3},q_auto/v1501870222/assets/btn-add-content_3x.png 3x
+              `}
+              alt={`${name}에 스티커 메세지를 남겨보세요`}
+              height={buttonSize}
+            />
+          </StyledImageButton>
+        </ARContainer>,
+        this.object.element,
+      );
     }
-
-    const diagonal = clamp(realDiagonal, MIN_DIAGONAL, MAX_DIAGONAL);
-    const realToClamped = realDiagonal / diagonal;
-
-    const nameTmp = document.createElement('template');
-    const buttonTmp = document.createElement('template');
-
-    nameTmp.innerHTML = renderToString(
-      <MessageText size={diagonal}>
-        <div>
-          {name}에
-        </div>
-
-        <div>
-          스티커 메세지를 남겨보세요
-        </div>
-      </MessageText>,
-    );
-
-    buttonTmp.innerHTML = renderToString(
-      <ImageButton type="button">
-        <img
-          src={addIcon}
-          srcSet={`${addIcon2x} 2x, ${addIcon3x} 3x`}
-          alt={`${name}에 스티커 메세지를 남겨보세요`}
-          width={diagonal * 0.33}
-        />
-      </ImageButton>,
-    );
-
-    const nameElem = nameTmp.content.firstChild;
-    const buttonElem = buttonTmp.content.firstChild;
-
-    buttonElem.addEventListener('click', onNewClick);
-
-    const nameAR = new DOMRenderable(nameElem);
-    const buttonAR = new DOMRenderable(buttonElem);
-
-    if (realDiagonal !== diagonal) {
-      nameAR.scale.setScalar(realToClamped);
-      buttonAR.scale.setScalar(realToClamped);
-    }
-
-    if (depth !== null && typeof depth !== 'undefined') {
-      nameAR.position.setZ(depth / 2);
-      buttonAR.position.setZ(depth / 2);
-    }
-
-    entity.addRenderable(nameAR);
-    nameAR.position.setY((height + (nameElem.clientHeight * realToClamped)) / 2 + realDiagonal * 0.04);
-    entity.addRenderable(buttonAR);
   }
 
   render() {
@@ -156,16 +153,19 @@ class Entity extends Component {
 
     return (
       <div {...other}>
-        <StyledImageButton
+        <StyledImageButtonRight
           type="button"
           onTouchEnd={onNewClick}
         >
           <img
             alt={`${name}에 스티커 메세지를 남겨보세요`}
-            src={createIcon}
-            srcSet={`${createIcon2x} 2x, ${createIcon3x} 3x`}
+            src="https://res.cloudinary.com/df9jsefb9/image/upload/c_scale,q_auto,w_72/v1501868965/assets/btn-create_3x.png"
+            srcSet="
+              https://res.cloudinary.com/df9jsefb9/image/upload/c_scale,q_auto,w_144/v1501868965/assets/btn-create_3x.png 2x,
+              https://res.cloudinary.com/df9jsefb9/image/upload/c_scale,q_auto,w_216/v1501868965/assets/btn-create_3x.png 3x
+            "
           />
-        </StyledImageButton>
+        </StyledImageButtonRight>
       </div>
     );
   }
