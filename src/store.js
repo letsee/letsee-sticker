7712d6dfbf705909cde637436f7b6d9e703c26ef
Yoonji Jang <yjang@letsee.io>
2017-08-31 16:03:30 +0900

    add redux-observable

--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import createSagaMiddleware from 'redux-saga';
-import { reactReduxFirebase } from 'react-redux-firebase';
+import { createEpicMiddleware } from 'redux-observable';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import firebase from './firebase';
 import reducers from './reducers';
+import epics from './epics';
 import initialState from './initialState';
 
 let preloadedState = initialState;
@@ -18,10 +20,16 @@ if (typeof window !== 'undefined' && window !== null && window.__PRELOADED_STATE
 }
 
 const sagaMiddleware = createSagaMiddleware();
+const epicMiddleware = createEpicMiddleware(epics, {
+  dependencies: {
+    getFirebase,
+  },
+});
 
 const enhancers = composeWithDevTools(
   reactReduxFirebase(firebase, { enableLogging: process.env.NODE_ENV !== 'production' }),
   applyMiddleware(sagaMiddleware),
+  applyMiddleware(epicMiddleware),
 );
 
 const store = createStore(reducers, preloadedState, enhancers);
