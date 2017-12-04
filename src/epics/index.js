2899ddf3a8550dd6918a636c8b43dbd0c38c2d3f
Yoonji Jang <yjang@letsee.io>
2017-12-04 12:56:26 +0900

    Entities flow

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,10 +1,12 @@
 // @flow
 import { combineEpics } from 'redux-observable';
 import { logInUser, logOutUser } from './currentUser';
+import entities from './entities';
 
 const epics = combineEpics(
   logInUser,
   logOutUser,
+  entities,
 );
 
 export default epics;
