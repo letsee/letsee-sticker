a03ca520eaae18cf4dd5235d2733e1a942dad0a4
JT Kang <jt.kang@letsee.io>
2019-05-22 14:16:43 +0900

    Update code: upload image with base64

---
+++ wallboard/thumb.js
@@ -0,0 +1,23 @@
+
+
+const CreateThumbnail = {
+  init: function(file) {},
+  getImage: function(_file) {
+    const file = _file;
+    const fileReader = new FileReader();
+    return new Promise((resolve, reject) => {
+      if (file.type.match('image')) {
+        fileReader.onload = function() {
+          // var img = document.createElement('img');
+          // img.src = fileReader.result;
+          // document.getElementsByTagName('div')[0].appendChild(img);
+          resolve(fileReader.result);
+        };
+        fileReader.readAsDataURL(file);
+      } else {
+        reject("please upload image only");
+      }
+
+    })
+  }
+}
