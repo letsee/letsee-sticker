773d21021a9c4aaafce57eeb6d532a522fc1a689
Jungwoo Lee <jjjjjw910911@letsee.io>
2019-05-24 01:38:50 +0900

    Resizing Image and make resized image blob file

--- wallboard/thumb.js
+++ wallboard/thumb.js
@@ -1,5 +1,3 @@
-
-
 const CreateThumbnail = {
   init: function(file) {},
   getImage: function(_file) {
@@ -17,54 +15,6 @@ const CreateThumbnail = {
       } else {
         reject("please upload image only");
       }
-
-    })
-  },
-  getImageResize: function (_file) {
-    const file = _file;
-    var fileReader = new FileReader();
-
-    return new Promise((resolve, reject) => {
-      if (file.type.match('image')) {
-        fileReader.onload = function (e) {
-          let img = document.createElement('img');
-          img.src = e.target.result;
-
-          var canvas = document.createElement('canvas');
-          var ctx = canvas.getContext('2d');
-          ctx.drawImage(img, 0, 0);
-
-          var MAX_WIDTH = 400;
-          var MAX_HEIGHT = 400;
-          var width = img.width;
-          var height = img.height;
-
-          if (width > height) {
-            if (width > MAX_WIDTH) {
-              height *= MAX_WIDTH / width;
-              width = MAX_WIDTH;
-            }
-          } else {
-            if (height > MAX_HEIGHT) {
-              width *= MAX_HEIGHT / height;
-              height = MAX_HEIGHT;
-            }
-          }
-          canvas.width = width;
-          canvas.height = height;
-
-          var ctx = canvas.getContext("2d");
-          ctx.drawImage(img, 0, 0, width, height);
-
-          let dataurl;
-          dataurl = canvas.toDataURL(file.type);
-          resolve(dataurl);
-        };
-        fileReader.readAsDataURL(file);
-      } else {
-        reject("please upload image only");
-      }
     })
   }
-};
 }
