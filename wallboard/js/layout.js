94ee1d4612e7c3cee0882941f9741658b3acedde
Jungwoo Lee <jjjjjw910911@letsee.io>
2019-05-24 23:28:38 +0900

    Create main layout and css and conference guide

--- wallboard/js/layout.js
+++ wallboard/js/layout.js
@@ -2,7 +2,6 @@ let w = window.innerWidth;
 let h = window.innerHeight;
 
 // set global elements.
-let addButton = document.getElementById("addButton");
 
 // set switchs.
 
@@ -13,17 +12,19 @@ let isOpenXButtonHelp = false;
 let isOpenInfoButtonHelp = true;
 
 let recommendDiv =  $("#recommendDiv");
+let inputArea = $("#inputArea");
 
-inputArea = $("#inputArea");
 
-// function to set layout when app initialize.
-function initLayoutPortrait() {
 
-  initOrientation();
+function initLayoutLandscape() {
 
-  addButton.style.top = ((h / 2) - ($(addButton).height() / 2)) + "px";
-  addButton.style.left = ((w / 2) - ($(addButton).width() / 2)) + "px";
-  addButton.style.display = "block";
+  w = window.innerWidth;
+  h = window.innerHeight;
+
+  //addButton.style.top = ((h / 2) - ($(addButton).height() / 2)) + "px";
+  //addButton.style.left = ((w / 2) - ($(addButton).width() / 2)) + "px";
+
+
 
   var m = parseInt(inputArea.css("margin-left"));
   var p = parseInt(inputArea.css("padding"));
@@ -35,12 +36,13 @@ function initLayoutPortrait() {
   let divHelp = document.getElementById("divHelp");
   //divHelp.style.width = w + "px";
   //divHelp.style.height = h + "px";
-  divHelp.style.width = w * 0.8 + "px";
-  divHelp.style.height = h * 0.9 + "px";
-  divHelp.style.marginLeft = w * 0.1 + "px";
-  divHelp.style.marginTop = h * 0.05 + "px";
+  //divHelp.style.width = w * 0.8 + "px"; 80%
+  //divHelp.style.height = h * 0.9 + "px"; 90%
+  //divHelp.style.marginLeft = w * 0.1 + "px"; 10%
+  //divHelp.style.marginTop = h * 0.05 + "px"; 5%
 
   // set xbutton
+  /*
   let xButtonHelp = document.getElementById("xButtonHelp");
   xButtonHelp.width = w * 0.1;
   xButtonHelp.style.marginLeft = w * 0.85 + "px";
@@ -48,21 +50,21 @@ function initLayoutPortrait() {
 
   // set info button
   let infoButtonHelp = document.getElementById("infoButtonHelp");
-  infoButtonHelp.width = w * 0.1;
+  infoButtonHelp.width = w * 0.08;
   infoButtonHelp.style.marginLeft = w * 0.85 + "px";
-  infoButtonHelp.style.marginTop = h * 0.025 +"px";
-
+  infoButtonHelp.style.marginTop = h * 0.05 +"px";
+  */
 }
-
-function initLayoutLandscape() {
-  initOrientation();
-}
-
 
 function initOrientation() {
+
+  // 세로모드일시
   if(window.orientation === 0) {
     recommendDiv.fadeIn();
   } else {
+
+    // 가로모드일시 레이아웃 초기화
+    initLayoutLandscape();
     recommendDiv.fadeOut();
   }
 }
@@ -132,20 +134,15 @@ function showInputConfirmDiv() {
 $(window).on("orientationchange",function(){
   if(window.orientation == 0) // Portrait
   {
-    w = window.innerWidth;
-    h = window.innerHeight;
     //setTimeout(initLayout, 100);
     recommendDiv.width = "100%";
     recommendDiv.height = "100%";
     recommendDiv.fadeIn();
-
   }
   else // Landscape
   {
-     w = window.innerWidth;
-    h = window.innerHeight;
     //setTimeout(initLayout, 100);
     recommendDiv.fadeOut();
-
+    initLayoutLandscape();
   }
 });
